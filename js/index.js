0e8e1f158b0c83e10299432728c04b8ef6b8dad4
Josaphat Mutale <mutalejosaphat@gmail.com>
2023-06-19 00:35:21 +0100

    Finition de la page fourchette

---
+++ js/index.js
@@ -0,0 +1,8 @@
+// let carre = document.querySelector('.carre');
+// let blackBloc = document.querySelector('.black');
+// let redBloc = document.querySelector('.red');
+// carre.addEventListener('click', (e) => {
+//     blackBloc.classList.add('yellowBloc');
+//     redBloc.classList.add('pinkBloc');
+// })
+
